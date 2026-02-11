608a02e8b0a2f6d8fc836946e2d16b25a7933416
Lakshmanan Palaniappan <154072727+Lakshmanan-palaniappan@users.noreply.github.com>
2026-02-11 13:25:45 +0530

    feat: While Loop

--- test.py
+++ test.py
@@ -2,9 +2,8 @@ print("Test Python File")
 print("1")
 print("2")
 print("3")
-for i in range(10):
-    print("i is printing 10 times",i)
-
-    print("Hello World")
-
-print("")
+n=int(input("Enter a Number: "))
+i=1
+while(i<=n):
+    print("i is printing",n*i)
+    i+=1
